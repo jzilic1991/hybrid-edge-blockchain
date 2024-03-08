446a62ab38b2ee842116a976d8422d78ee31d861
Josip Zilic <josip.zilic@tuwien.ac.at>
2024-03-08 13:58:36 +0100

    Graph topology

--- src/topology_plotter.py
+++ src/topology_plotter.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
-from topology_generator import parse_topology_file, cluster_cells
+import networkx as nx
+from topology_generator import *
 
 
 def plot_topology (data, labels):
@@ -7,13 +8,13 @@ def plot_topology (data, labels):
     lats = list ()
     longs = list ()
     
-    for gps_coord in parsed_data.values ():
+    for gps_coord in data:
 
-        lats.append (gps_coord ["latitude"])
-        longs.append (gps_coord ["longitude"])
+        lats.append (gps_coord [0])
+        longs.append (gps_coord [1])
 
     plt.figure(figsize = (10, 6))
-    plt.scatter(longs, lats, c=labels, cmap='viridis', alpha=0.5)
+    plt.scatter(longs, lats, c = labels, cmap = 'viridis', alpha = 0.5)
     plt.xlabel('Longitude')
     plt.ylabel('Latitude')
     plt.title('Clustered Cells')
@@ -21,8 +22,17 @@ def plot_topology (data, labels):
     plt.show()
 
 
+def plot_graph (graph):
+
+    nx.draw(graph, with_labels = False, node_color = 'skyblue', node_size = 1000, font_size = 12, font_weight = 'bold')
+    plt.show()
+
+
 parsed_data = parse_topology_file ('data/232.csv')
 data, labels = cluster_cells (parsed_data, num_clusters = 30)
-# plot_topology (data, labels)
-print ("Labels: " + str (labels))
+cluster_nodes = create_cluster_nodes (data, labels)
+graph = create_graph (cluster_nodes)
+plot_topology (data, labels)
+# plot_graph (graph)
+# print ("Labels: " + str (labels))
 # print ("Data: " + str (data))
